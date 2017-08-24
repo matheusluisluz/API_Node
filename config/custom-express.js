7d52249534ead143bfe57a1d3ace699590ddc391
logonrm <logonrm@fiap.com.br>
2017-08-23 22:18:10 -0300

    Body parser

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,8 +1,12 @@
 var express = require("express"); //carrega o arquivo
-var consign = require("consign");//auxilia no carregamento das rotas
+var consign = require("consign");//auxilia no carregamento das 
+var bodyParser = require("body-parser");
 
 module.exports = function () {
     var app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
 
     consign()
         .include("/controllers")
--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,6 +1,12 @@
-module.exports = function(app){
-    app.get("/pagamentos",function(request,response){
+module.exports = function (app) {
+    app.get("/pagamentos", function (request, response) {
         console.log("Recebida requisicao de teste na porta 3000");
+        response.send("OK");
+    })
+
+    app.post("/pagamentos/pagamento", function (request, response) {
+        var pagamento = request.body();
+        console.log(pagamento);
         response.send("OK");
     })
 };
